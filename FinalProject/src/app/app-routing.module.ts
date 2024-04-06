cdf0a35392be5a136f6cf63742331576f1805bea
Palat7 <152816239+Palat7@users.noreply.github.com>
2024-04-06 16:50:55 -0400

    Ajout de la route vers le panier

--- FinalProject/src/app/app-routing.module.ts
+++ FinalProject/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListeVetmentsComponent } from './liste-vetments/liste-vetments.component';
 import { HomeComponent } from './home/home.component';
 import { DetailsVetmentComponent } from './details-vetment/details-vetment.component';
+import {AchatComponent} from "./achat/achat.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'collection', component: ListeVetmentsComponent },
+  { path: 'achat', component: AchatComponent },
   { path: 'detail/:id', component: DetailsVetmentComponent },
 ];
 
